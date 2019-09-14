90df19ce80a57eccaa3fa9c4100de615c9ec3992
yl131 <53383107+yl131@users.noreply.github.com>
2019-09-14 14:43:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,46 +5,35 @@ import simplejson as json
 from bokeh.plotting import figure
 from bokeh.palettes import Spectral11
 from bokeh.embed import components 
+import api_pull as ap
+import plot
 
 app = Flask(__name__)
+
 app.vars={}
 
+@app.route('/')
+def main():
+    return redirect('/index')
 
-#@app.route('/')
-#def index():
-#  return render_template('index.html')
+@app.route('/index',methods=['GET','POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        #request was a post
+        app.vars['ticker'] = request.form['ticker']
+        app.vars['results'] = ap.get_data(app.vars['ticker'])
+        print("length of df: ",len(app.vars['results']))
+        if len(app.vars['results']) == 0:
+            return render_template('error.html')
+        else:
+            script, div = plot.fig(app.vars['results'], app.vars['ticker'])
+            f = open('%s.txt'%(app.vars['ticker']),'w')
+            f.write('Ticker: %s\n'%(app.vars['ticker']))
+            f.close()
+            return render_template('results.html', script=script, div=div)
 
-#@app.route('/about')
-#def about():
-  #return render_template('about.html')
 
-def graph():
-  app.vars['ticker'] = request.form['ticker']
-
-  api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=gVz7XbzeecyxHdkCn8yB' % app.vars['ticker']
-  session = requests.Session()
-  session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-  raw_data = session.get(api_url)
-
-  a = raw_data.json()
-  df = pandas.DataFrame(a['data'], columns=a['column_names'])
-
-  df['Date'] = pandas.to_datetime(df['Date'])
-
-  p = figure(title='Stock prices for %s' % app.vars['ticker'],
-      x_axis_label='date',
-      x_axis_type='datetime')
-
-  if request.form.get('Close'):
-      p.line(x=df['Date'].values, y=df['Close'].values,line_width=2, legend='Close')
-  if request.form.get('Adj. Close'):
-      p.line(x=df['Date'].values, y=df['Adj. Close'].values,line_width=2, line_color="green", legend='Adj. Close')
-  if request.form.get('Open'):
-      p.line(x=df['Date'].values, y=df['Open'].values,line_width=2, line_color="red", legend='Open')
-  if request.form.get('Adj. Open'):
-      p.line(x=df['Date'].values, y=df['Adj. Open'].values,line_width=2, line_color="purple", legend='Adj. Open')
-  script, div = components(p)
-  return render_template('graph.html', script=script, div=div)
-
-if __name__ == '__main__':
-    app.run(port=33507)
+if __name__ == "__main__":
+    app.run(debug=True)
