34d8603b3f8a902eb088e0b8db56cdc2c187fe40
yl131 <53383107+yl131@users.noreply.github.com>
2019-09-14 15:20:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -73,11 +73,5 @@ def plot_price():
     script, div = components(plot)
     return render_template('graph.html', script=script, div=div)
 
-
-#if __name__ == '__main__':
-  #app.run(port=33507)
-
-# Binding PORT for Heroku deployment
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+  app.run(port=33507)
