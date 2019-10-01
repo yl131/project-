e574cf1f68e0c71273aec8968adb340aadcdcf0a
yl131 <53383107+yl131@users.noreply.github.com>
2019-10-01 14:18:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
 import requests
+import os
 import pandas as pd
 from flask import Flask, render_template, request
-from bokeh.plotting import figure
-from bokeh.embed import components
+from ocr_core import ocr_core
+
+UPLOAD_FOLDER = '/static/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET','POST'])
 def index():
 	return render_template('index.html')
 
+def upload_page():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return render_template('index.html', msg='No file selected')
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return render_template('index.html', msg='No file selected')
 
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
+
+            # call the OCR function on it
+            extracted_text = ocr_core(file)
+
+            # extract the text and display it
+            return render_template('upload.html',
+                                   msg='Successfully processed',
+                                   extracted_text=extracted_text,
+                                   img_src=UPLOAD_FOLDER + file.filename)
+    elif request.method == 'GET':
+        return render_template('upload.html')
 
 if __name__ == '__main__':
-	app.run(port=33507)
+    app.run()
