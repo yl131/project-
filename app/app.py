6df9b8a48095073efbee5c270964b8a86c3ed2ba
yl131 <53383107+yl131@users.noreply.github.com>
2019-10-09 21:04:07 -0400

    Update app.py
    Changed the template docker app.py to include prediction model

--- app/app.py
+++ app/app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template, request, redirect
+import requests
+import os
+import pandas as pd
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/')
+model = pickle.load(open('lib/models/wine_estimator.pkl','rb'))
+    
+@app.route('/', methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/prediction', methods=['POST'])
+def predict():
+    
+    query = {}
+    query['description'] = request.form['description']
+    query['price'] = request.form['price']
+    query['province'] = request.form['province']
+    query['variety'] = request.form['variety']
+    query['winery'] = request.form['winery']
+    
+    query_df = pd.DataFrame.from_dict(query)
+    prediction = model.predict(query_df)
+     
+    return render_template('prediction.html', prediction = prediction)
+
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run()
